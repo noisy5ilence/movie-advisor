48f43afef237e723bec09795c350786c96f457c7
ner <inerono@gmail.com>
2024-03-25 16:59:49 +0200

    fix: modal animation

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -41,7 +41,7 @@ const DialogContent = React.forwardRef<
         ref={ref}
         {...props}
         className={cn(
-          'absolute top-[20vh] max-w-[1000px] w-[calc(100%-24px)] left-[50%] rounded-lg translate-x-[-50%] gap-4 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=open]:slide-in-from-left-1/2 bg-background',
+          'absolute top-[20vh] max-w-[1000px] w-[calc(100%-24px)] left-[50%] rounded-xl translate-x-[-50%] gap-4 shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=open]:slide-in-from-left-1/2 bg-background',
           className
         )}
       >
