62791957bfd04f0c6eea9d8cd4251ca573cc7e86
ner <inerono@gmail.com>
2024-10-29 08:47:08 +0200

    fix: server's root path

--- data/clients/movieAdvisor.ts
+++ data/clients/movieAdvisor.ts
@@ -1,13 +1,10 @@
 import { isServer } from '@tanstack/react-query';
 
+import { SITE_URL } from '@/env';
+
 import Http from './Http';
 
-const BASE =
-  process.env.NEXT_VERCEL_ENV === 'production'
-    ? process.env.NEXT_VERCEL_PROJECT_PRODUCTION_URL
-    : process.env.NEXT_VERCEL_URL;
-
-const ROOT = isServer ? `${BASE?.startsWith('http') ? BASE : `https://${BASE}`}` : '';
+const ROOT = isServer ? `${SITE_URL?.startsWith('http') ? SITE_URL : `https://${SITE_URL}`}` : '';
 
 const movieAdvisor = new Http(`${ROOT}/api/tmdb`);
 
