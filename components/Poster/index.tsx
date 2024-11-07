ef6223ff032e6c51aed405b337423c1a3b16eec8
ner <inerono@gmail.com>
2024-11-07 20:07:52 +0200

    fix: robots.txt

--- components/Poster/index.tsx
+++ components/Poster/index.tsx
@@ -19,12 +19,17 @@ const Poster: FC<Props> = ({ show, onClick, className, lazy = true }) => (
       className
     )}
   >
-    <picture className='size-full rounded-lg object-cover md:rounded-none md:rounded-l-lg'>
+    <picture>
       <source srcSet={show.poster['1x']} media='(max-resolution: 191dpi)' />
       <source srcSet={show.poster['1x']} media='(max-device-pixel-ratio: 1.99)' />
       <source srcSet={show.poster['1.5x']} media='(min-resolution: 192dpi)' />
       <source srcSet={show.poster['1.5x']} media='(min-device-pixel-ratio: 2)' />
-      <img className='size-full' loading={lazy ? 'lazy' : 'eager'} src={show.poster['1x']} alt={show.title} />
+      <img
+        className='size-full rounded-lg object-cover md:rounded-none md:rounded-l-lg'
+        loading={lazy ? 'lazy' : 'eager'}
+        src={show.poster['1x']}
+        alt={show.title}
+      />
     </picture>
     <Handlers show={show} onClick={onClick} />
   </div>
---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+      { userAgent: '*', disallow: '/favorites' },
+      { userAgent: '*', disallow: '/watch-list' }
+    ]
+  }
+};
