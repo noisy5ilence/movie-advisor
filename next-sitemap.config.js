a3fb95eca1221407b08e3ed1067f8ceb3df44235
ner <inerono@gmail.com>
2024-11-07 20:14:12 +0200

    fix: robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
+  siteUrl: `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`,
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
