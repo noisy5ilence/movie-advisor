063a2494aec1d2aef259f8679726004040991a9e
ner <inerono@gmail.com>
2024-11-16 18:50:18 +0200

    feat: show page meta

--- app/(site)/[...show]/page.tsx
+++ app/(site)/[...show]/page.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 
 import detailsQuery from '@/data/queries/details';
+import { SITE_URL, TITLE } from '@/env';
 import getQueryClient from '@/lib/queryClient';
 
 import Container from './container';
@@ -10,6 +12,32 @@ import Container from './container';
 interface Props {
   params: { show: [Show['type'], Show['id']] };
 }
+
+export const generateMetadata = async ({ params: { show } }: Props): Promise<Metadata> => {
+  const queryClient = getQueryClient();
+
+  const [showType, showId] = show as [Show['type'], Show['id']];
+
+  if (!showType || !showId) return redirect('/');
+
+  try {
+    const { title, overview, poster } = await queryClient.fetchQuery(detailsQuery({ showId, showType }));
+
+    return {
+      title: `${title} | ${TITLE}`,
+      description: overview,
+      openGraph: {
+        title: `${title} | ${TITLE}`,
+        description: overview,
+        images: [poster['2x']],
+        type: 'website',
+        url: `${SITE_URL}/${showType}/${showId}`
+      }
+    };
+  } catch (_) {
+    return redirect('/');
+  }
+};
 
 const ShowPage: FC<Props> = async ({ params: { show } }) => {
   const queryClient = getQueryClient();
--- components/Preview/components/Torrents/components/TorrentsTable/components/Actions/index.tsx
+++ components/Preview/components/Torrents/components/TorrentsTable/components/Actions/index.tsx
@@ -56,12 +56,13 @@ const Actions: FC<Props> = ({ torrent, backdrop, title, provider }) => {
         <Button variant='outline' className='relative grow-0 px-3' title='Download m3u playlist'>
           <a
             className='absolute left-0 top-0 size-full'
+            target='_blank'
             href={`${STREAM_URL}?m3u&link=${encodeURIComponent(magnet)}`}
           />
           <ListVideo size={20} />
         </Button>
         <Button className='relative grow-0 px-3' variant='outline' title='Download magnet'>
-          <a className='absolute left-0 top-0 size-full' href={magnet} />
+          <a className='absolute left-0 top-0 size-full' target='_blank' href={magnet} />
           <Magnet size={20} />
         </Button>
         {supportedForCast && (
