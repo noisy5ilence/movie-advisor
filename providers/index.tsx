7634141c5086477b788e062b9fce618dbfae76f2
ner <inerono@gmail.com>
2026-09-25 16:06:00 +0300

    perf(hydration): drop ReactQueryStreamedHydration
    HydrationBoundary hydrates the global context client, so __RQ was
    re-serializing the same dehydrated state into every page's HTML (10-15KB
    per page, 149KB on /person) and then hydrating the same client again.
    The boundary is now the single serialization and hydration path.

--- providers/index.tsx
+++ providers/index.tsx
@@ -4,7 +4,6 @@ import { FC, ReactNode } from 'react';
 import ModalContainer from 'react-modal-promise';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 import { ThemeProvider } from 'next-themes';
 
 import { Toaster } from '@/components/ui/toaster';
@@ -19,9 +18,7 @@ const Providers: FC<Props> = ({ children }) => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryStreamedHydration>
-        <ThemeProvider attribute='data-mode'>{children}</ThemeProvider>
-      </ReactQueryStreamedHydration>
+      <ThemeProvider attribute='data-mode'>{children}</ThemeProvider>
       <ModalContainer exitTimeout={0} enterTimeout={0} />
       <Toaster />
       <ReactQueryDevtools initialIsOpen={false} />
